f09492497e12336ef96bddd1dea6205f423f71fb
Yatoute <mintoamayatoute@gmail.com>
2025-11-28 23:13:57 +0100

    FEAT : GPT Tokenizer

--- gptlight/src/gptlight/tokenizer/__init__.py
+++ gptlight/src/gptlight/tokenizer/__init__.py
@@ -1,10 +1,34 @@
+from __future__ import annotations
+
+from typing import Literal, Collection, AbstractSet
+
 import tiktoken
+import torch
 
 from .simple_tokenizer import SimpleTokenizer
 
-def gpt_encoding():
+class GPTTokenizer():
     
-    """
-    GPT 2 encoding using Tiktoken
-    """
-    return tiktoken.get_encoding("gpt2")
+    def __init__(self, encoding_name:str="gpt2"):
+        
+        self.encoding_name = encoding_name
+        self.encoding = tiktoken.get_encoding(encoding_name)
+            
+    def encode(self, 
+            text:str, 
+            *, 
+            allowed_special: AbstractSet[str] | Literal['all'] = {'<|endoftext|>'}, 
+            disallowed_special: Collection[str] | Literal['all'] = "all"
+        ) -> torch.Tensor:
+        
+        encoded = self.encoding.encode(text, allowed_special, disallowed_special)
+        self.encoding.decode()
+        return torch.tensor(encoded).unsqueeze(0)
+        
+    def decode(self, 
+        tokens:torch.Tensor,
+        errors: str = "replace"
+    ) -> str:
+        
+        flat = tokens.squeeze(0)
+        return self.decode(flat.tolist(), errors)
--- gptlight/src/gptlight/utils/__init__.py
+++ gptlight/src/gptlight/utils/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import tiktoken
+import torch
 from torch.utils.data import DataLoader
 from gptlight.utils.data import GPTDataset
 
@@ -16,3 +17,16 @@ def create_dataloader(txt, batch_size=4, max_length=256, stride=128, shuffle=Tru
     )
 
     return dataloader
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+        
+    return idx
